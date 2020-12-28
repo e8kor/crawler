a6e8e6848978db46261dcde24f36344737917720
IEvgenii Korniichuk <nutscracker.ua@gmail.com>
2020-12-28 23:38:51 +0100

    introduced persist function

---
+++ persistor/handler.go
@@ -0,0 +1,59 @@
+package function
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+// Entry is domain associated crawled json
+type Entry struct {
+	domain string
+	data   map[string]interface{}
+}
+
+// Handle a serverless request
+func Handle(req []byte) string {
+	var (
+		host     = os.Getenv("PG_HOST")
+		port     = os.Getenv("PG_PORT")
+		user     = os.Getenv("PG_USER")
+		password = os.Getenv("PG_PASSWORD")
+		dbname   = os.Getenv("PG_DBNAME")
+		payload  Entry
+		result   sql.Result
+		id       int64
+	)
+
+	err := json.Unmarshal([]byte(req), payload)
+	if err != nil {
+		panic(err)
+	}
+
+	info := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", info)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	preparedQuery := fmt.Sprintf(`INSERT INTO %s (data) VALUES (%s)`, payload.domain, payload.data)
+	result, err = db.Exec(preparedQuery)
+	if err != nil {
+		panic(err)
+	}
+
+	id, err = result.LastInsertId()
+	if err != nil {
+		panic(err)
+	}
+
+	response := fmt.Sprintf(`{
+    "status": true,
+    "id": "%s"
+  }`, id)
+	return response
+}
