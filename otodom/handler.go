f5f3bc2963ff8040da4572053a4ef18a29cc4c4a
IEvgenii Korniichuk <nutscracker.ua@gmail.com>
2020-12-29 22:10:31 +0100

    updated env var

--- otodom/handler.go
+++ otodom/handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gocolly/colly/v2"
 )
@@ -19,32 +20,34 @@ type Entry struct {
 }
 
 func Handle(w http.ResponseWriter, r *http.Request) {
-	c := colly.NewCollector()
-	entries := make([]Entry, 0, 20)
 	URL := r.URL.Query().Get("url")
+	if URL == "" {
+		URL = os.Getenv("SOURCE_URL")
+	}
 	if URL == "" {
 		log.Fatalln("{ \"error\": \"missing url parameter\"}")
 		w.Write([]byte("[]"))
-		return
+	} else {
+		c := colly.NewCollector()
+		entries := make([]Entry, 0, 20)
+		c.OnHTML("article[id]", func(e *colly.HTMLElement) {
+			entry := Entry{
+				Title:  e.ChildText("div.offer-item-details > header > h3 > a > span > span"),
+				Name:   e.ChildText("div.offer-item-details-bottom > ul > li"),
+				Region: e.ChildText("div.offer-item-details > header > p"),
+				Price:  e.ChildText("div.offer-item-details > ul > li.hidden-xs.offer-item-price-per-m"),
+				Area:   e.ChildText("div.offer-item-details > ul > li.hidden-xs.offer-item-area"),
+				Link:   e.ChildAttr("div.offer-item-details > header > h3 > a", "href"),
+			}
+			entries = append(entries, entry)
+		})
+
+		c.OnRequest(func(r *colly.Request) {
+			log.Println("visiting", r.URL.String())
+		})
+
+		c.Visit(URL)
+		enc := json.NewEncoder(w)
+		enc.Encode(entries)
 	}
-
-	c.OnHTML("article[id]", func(e *colly.HTMLElement) {
-		entry := Entry{
-			Title:  e.ChildText("div.offer-item-details > header > h3 > a > span > span"),
-			Name:   e.ChildText("div.offer-item-details-bottom > ul > li"),
-			Region: e.ChildText("div.offer-item-details > header > p"),
-			Price:  e.ChildText("div.offer-item-details > ul > li.hidden-xs.offer-item-price-per-m"),
-			Area:   e.ChildText("div.offer-item-details > ul > li.hidden-xs.offer-item-area"),
-			Link:   e.ChildAttr("div.offer-item-details > header > h3 > a", "href"),
-		}
-		entries = append(entries, entry)
-	})
-
-	c.OnRequest(func(r *colly.Request) {
-		log.Println("visiting", r.URL.String())
-	})
-
-	c.Visit(URL)
-	enc := json.NewEncoder(w)
-	enc.Encode(entries)
 }
