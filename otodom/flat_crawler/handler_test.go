e881d820e3eada4659bfe722a8813e041f196fd0
IEvgenii Korniichuk <nutscracker.ua@gmail.com>
2021-02-27 18:04:16 +0100

    updated

--- otodom/flat_crawler/handler_test.go
+++ otodom/flat_crawler/handler_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestHandleReturnsCorrectResponse(t *testing.T) {
-	entries := CollectEntries("https://www.otodom.pl/wynajem/mieszkanie/krakow/?search%5Bcity_id%5D=38")
+	entries, _ := CollectEntries("https://www.otodom.pl/wynajem/mieszkanie/krakow/?search%5Bcity_id%5D=38")
 
 	if len(entries) == 0 {
 		t.Fatal("Expected entries to be non empty")
